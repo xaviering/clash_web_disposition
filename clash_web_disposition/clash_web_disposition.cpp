3bb3dc27e3dafdf3a7928be2c7ea915c6a8df3ca
xaviering@outlook.com <xaviering@outlook.com>
2021-04-15 19:26:01 +0800

    Add project files.

---
+++ clash_web_disposition/clash_web_disposition.cpp
@@ -0,0 +1,38 @@
+// clash_web_disposition.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+typedef std::basic_string <char> string;
+int main()
+{
+	string a =
+		"https://subcon.dlj.tf/sub?target=clash&new_name=true&url=https%3A%2F%2Fraw.sevencdn.com%2FHEU-F8-PRACTICE%2Fstor%2Fmaster%2F20210202%2F52da1e2e1c8701df5f285877516b2495%2F52da1e2e1c8701df5f285877516b2495.txt&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini";
+	string b = "https://raw.githubusercontent.com/ssrsub/ssr/master/Clash.yml";
+	string c =
+		"https://subcon.dlj.tf/sub?target=clash&new_name=true&url=https%3A%2F%2Fonedrive.gimhoy.com%2F1drv%2FaHR0cHM6Ly8xZHJ2Lm1zL3QvcyFBakdEc3oydC1PRFJnZ2tnMXBRTXRkbmpqZE00P2U9SzhkNjd5&insert=false&config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini";
+	string d = "https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/0207clash.yml";
+	string e =
+		"https://subcon.dlj.tf/sub?config=https%3A%2F%2Fraw.githubusercontent.com%2FACL4SSR%2FACL4SSR%2Fmaster%2FClash%2Fconfig%2FACL4SSR_Online.ini&new_name=true&url=https%3A%2F%2Fonedrive.gimhoy.com%2F1drv%2FaHR0cHM6Ly8xZHJ2Lm1zL3QvcyFBakdEc3oydC1PRFJnZ3Npank1MjMtQ0p4dWduP2U9eUlhb0ZW&target=clash&insert=false";
+	string f =
+		"https://ugcatt.smcdn.cn/rquDITqKNibrVwcf.txt?auth_key=1618485626-ly8PURJfsjNQ18Qy-0-b0dc89bccc98610ab02212fd321b3d8c&response-content-disposition=inline%3B+filename%3D%22TG%2540baipiaojiedian.txt%22%3B+filename%2A%3DUTF-8%27%27TG%2540baipiaojiedian.txt";
+	string g = "https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/0313clash.yml";
+	string h =
+		"https://ugcatt.smcdn.cn/JoZP6SksWqOH1QdS.txt?auth_key=1618485677-Jq3UrARzkdRFuo8T-0-cb86fd7b52bd13929b8f65719dc2a6b1&response-content-disposition=inline%3B+filename%3D%22TG%2540baipiaojiedian.txt%22%3B+filename%2A%3DUTF-8%27%27TG%2540baipiaojiedian.txt";
+	string i =
+		"https://api.dler.io/sub?target=clash&new_name=true&url=https%3A%2F%2Fraw.githubusercontent.com%2Fkrsipig%2F2021.03.06.%2Fmain%2FREADME.md&insert=false";
+	
+	
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
